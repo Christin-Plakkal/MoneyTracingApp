580cc06a6e6112b3db48851bb26f44dcc53bf25a
Christin <christinplakkal@gmail.com>
2026-04-18 15:44:44 +0530

    config changes

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export const CONFIG = {
   APPS_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbzFCeLZnOXa5YbKz5QqLKQRmQmOJt_EFycC7FRHUpE8cZ7ofnAAZ51HLVGNNdFjfeagJg/exec',
   ALLOWED_EMAIL: 'christinplakkal@gmail.com',
-  GOOGLE_CLIENT_ID: 'YOUR_GOOGLE_CLIENT_ID.apps.googleusercontent.com'
+  GOOGLE_CLIENT_ID: '251105028969-5c6al6mcun51k0441oj4klrdle2dgn88.apps.googleusercontent.com'
 };
